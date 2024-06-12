6552da1f8e00402ead800c5d301562f3c58f605e
Jair Leonardo Jimenez Munevar <jairmunevar@gmail.com>
2024-06-12 17:53:05 -0500

    Initial package structure

---
+++ app/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,31 @@
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
+
+import { products } from '../data/products';
+
+const product = products[0];
+
+
+export const ShoppingPage = () => {
+    return (
+        <div>
+            <h1>Shopping Store</h1>
+            <hr />
+
+            <ProductCard
+                key={product.id}
+                product={product}
+                initialValues={{
+                    count: 4,
+                    maxCount: 10
+                }}>
+                {({ count, isMaxCountReached, reset, increaseBy }) => (
+                    <>
+                        <ProductImage />
+                        <ProductTitle />
+                        <ProductButtons />
+                    </>
+                )}
+            </ProductCard>
+        </div>
+    )
+}
